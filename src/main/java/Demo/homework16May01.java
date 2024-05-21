02c68fe40984ca68971253614d653ad271fa5437
khabibovkhabib <habib.habiblee@gmail.com>
2024-05-21 09:55:14 +0400

    first lesson

---
+++ src/main/java/Demo/homework16May01.java
@@ -0,0 +1,36 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class homework16May01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Rəqəmi daxil edin");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Əməliyyatı seçin");
+        char operation = scanner.next().charAt(0);
+        System.out.println("ikinci rəqəmi daxil edin");
+        double secondNumber = scanner.nextDouble();
+        double  result= 0;
+        if (operation=='-'){
+            result= firstNumber-secondNumber;
+        }
+        else if(operation=='+'){
+            result= firstNumber+secondNumber;
+        }
+        else if(operation=='*'){
+            result= firstNumber*secondNumber;
+        }
+        else if(operation=='/'){
+            if (secondNumber!=0){
+                result=firstNumber/secondNumber;
+            }else {
+                System.out.println("0 - a bölmək mümkün deyil");
+            }
+        }
+        else {
+            System.out.println("Yanlış seçim");
+        }
+        System.out.println("Cavab: "+ result);
+    }
+}
---
+++ src/main/java/Demo/homework16May02.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class homework16May02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zəhmət olmazsa ilk fənndən olan nəticənizi qeyd edin:");
+        double firstExapResult= scanner.nextDouble();
+        System.out.println("Zəhmət olmazsa ikinci fənndən olan nəticənizi qeyd edin:");
+        double secondExapResult= scanner.nextDouble();
+        System.out.println("Zəhmət olmazsa üçüncü fənndən olan nəticənizi qeyd edin:");
+        double thirdExapResult= scanner.nextDouble();
+        double total= (firstExapResult+secondExapResult+thirdExapResult)/3;
+        System.out.println("orta balınız: "+ total);
+    }
+}
