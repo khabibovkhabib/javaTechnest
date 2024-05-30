041c55964e9df0481e8bacb6637809267c477005
khabibovkhabib <101251184+khabibovkhabib@users.noreply.github.com>
2024-05-30 15:41:26 +0400

    Update and rename homework30May.java to homework30May01.java
    task1 done

---
+++ src/main/java/Demo/homework30May01.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class homework30May01 {
+   public static void main(String[] args) {    
+        int[] numbers = {1, 2, 3, 4};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (j != i) {
+                    for (int k = 0; k < numbers.length; k++) {
+                        if (k != i && k != j) {
+                            System.out.println(numbers[i] + " " + numbers[j] + " " + numbers[k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
