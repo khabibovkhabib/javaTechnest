c40332651a2657f69173104bb272d363bfcef0c9
khabibovkhabib <101251184+khabibovkhabib@users.noreply.github.com>
2024-05-30 16:04:04 +0400

    Update homework30May01.java
    task1 created

--- src/main/java/Demo/homework30May01.java
+++ src/main/java/Demo/homework30May01.java
@@ -8,6 +8,7 @@ public class homework30May01 {
                 if (j != i) {
                     for (int k = 0; k < numbers.length; k++) {
                         if (k != i && k != j) {
+                           
                             System.out.println(numbers[i] + " " + numbers[j] + " " + numbers[k]);
                         }
                     }
