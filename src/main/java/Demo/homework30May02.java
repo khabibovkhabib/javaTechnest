70f8a315bc87a4ba1341644c9493dde6b6433e73
khabibovkhabib <101251184+khabibovkhabib@users.noreply.github.com>
2024-05-30 16:03:25 +0400

    Create homework30May02.java
    task 2 created

---
+++ src/main/java/Demo/homework30May02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class homework30May02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zəhmət olmazsa ilk rəqəmi daxil edin:");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Zəhmət olmazsa ikinci rəqəmi daxil edin:");
+        double secondNumber = scanner.nextDouble();
+        System.out.println("Maksimum rəqəm: " + findMax(firstNumber, secondNumber));
+        scanner.close();
+    }
+
+    public static double findMax(double num1, double num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+}
