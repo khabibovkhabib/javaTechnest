fa2d7caf346a17815dcf56d8d8e234e5905af7f1
khabibovkhabib <101251184+khabibovkhabib@users.noreply.github.com>
2024-05-30 16:42:06 +0400

    Update homework30May03.java
    Task3 done

--- src/main/java/Demo/homework30May03.java
+++ src/main/java/Demo/homework30May03.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class homework30May03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zəhmət olmazsa ilk rəqəmi daxil edin:");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Zəhmət olmazsa ikinci rəqəmi daxil edin:");
+        double secondNumber = scanner.nextDouble();
+        System.out.println("Rəqəmlərin cəmi: " + sum(firstNumber, secondNumber));
+        scanner.close();
+    }
+    public static double sum(double num1, double num2) {
+        double result = num1 + num2;
+        return result;
+    }
+}
