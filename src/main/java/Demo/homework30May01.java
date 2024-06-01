47700cb5b9d3fb81a2316b5660bc0620c2f2bc23
khabibovkhabib <habib.habiblee@gmail.com>
2024-06-01 16:58:15 +0400

    first lesson

--- src/main/java/Demo/homework30May01.java
+++ src/main/java/Demo/homework30May01.java
@@ -1,7 +1,7 @@
 package Demo;
 
 public class homework30May01 {
-   public static void main(String[] args) {    
+    public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4};
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers.length; j++) {
@@ -9,7 +9,7 @@ public class homework30May01 {
                     for (int k = 0; k < numbers.length; k++) {
                         if (k != i && k != j) {
 
-                            System.out.println(numbers[i] + " " + numbers[j] + " " + numbers[k]);
+                            System.out.println( "" + numbers[i] + numbers[j] + numbers[k]);
                         }
                     }
                 }
