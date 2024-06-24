a17eb0a23641aad3702c050f2e8bb86de533e943
khabibovkhabib <101251184+khabibovkhabib@users.noreply.github.com>
2024-06-24 11:47:05 +0400

    Update main.java
    main created

--- src/main/java/Demo/homeworks19June/main.java
+++ src/main/java/Demo/homeworks19June/main.java
@@ -1,2 +1,22 @@
-uptaded
-  
+package Demo.homeworks19June;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BankAccountApp account = new BankAccountApp(12345678, "Müştəri Adı", 0, true);
+
+        System.out.println("Zəhmət olmazsa artırmaq istədiyiniz məbləği daxil edin:");
+        double increaseAmount = sc.nextDouble();
+        account.increaseBalance(increaseAmount);
+        System.out.println("Balans: " + account.getBalance());
+
+        System.out.println("Zəhmət olmazsa ödəniş məbləğini daxil edin:");
+        double reduceAmount = sc.nextDouble();
+        account.reduceBalance(reduceAmount);
+        System.out.println("Balans: " + account.getBalance());
+
+        sc.close();
+    }
+}
