906024f4dc0b6b38debfbf5f95ef2388508c733a
khabibovkhabib <101251184+khabibovkhabib@users.noreply.github.com>
2024-06-24 12:01:07 +0400

    Update main.java

--- src/main/java/Demo/homeworks19June/main.java
+++ src/main/java/Demo/homeworks19June/main.java
@@ -1,3 +1,4 @@
+// Main.java
 package Demo.homeworks19June;
 
 import java.util.Scanner;
@@ -5,18 +6,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        BankAccountApp account = new BankAccountApp(12345678, "Müştəri Adı", 0, true);
-
+        BankAccountApp account = new BankAccountApp();
+        System.out.println("Zəhmət olmazsa hesab nömrəsini daxil edin:");
+        int accountNumber = sc.nextInt();
+        account.setAccountNumber(accountNumber);
+        System.out.println("Zəhmət olmazsa müştərinin adını daxil edin:");
+        String customerName = sc.nextLine();
+        account.setCustomerName(customerName);
+        account.setAccountActivity(true);
         System.out.println("Zəhmət olmazsa artırmaq istədiyiniz məbləği daxil edin:");
         double increaseAmount = sc.nextDouble();
         account.increaseBalance(increaseAmount);
         System.out.println("Balans: " + account.getBalance());
-
         System.out.println("Zəhmət olmazsa ödəniş məbləğini daxil edin:");
         double reduceAmount = sc.nextDouble();
         account.reduceBalance(reduceAmount);
         System.out.println("Balans: " + account.getBalance());
-
         sc.close();
     }
 }
